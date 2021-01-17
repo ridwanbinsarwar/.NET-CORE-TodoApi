9c83fd91538c59a5a9e217bf5189de48e9c61cab
xenon-r <38183049+xenon-r@users.noreply.github.com>
2021-01-17 22:38:35 +0600

    Add files via upload

---
+++ BusinessLogic/Entities/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Entities
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Task { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ BusinessLogic/Interfaces/ITodoData.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface ITodoData
+    {
+        void CreateTodo(Guid id, String task, String title, DateTime date);
+        List<Todo> GetTodos();
+        void UpdateTodo(Guid id, Todo todo);
+        void UpdateTodoDatetime(Guid id, DateTime date);
+        void DeleteTodo(Guid id);
+        List<Todo> GetTodoByDatetime(DateTime date);
+    }
+}
+
